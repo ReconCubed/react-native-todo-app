26cf196a58f0abe1868c38e5ab10db49d1de88bf
reconcubed <recongamingsam@gmail.com>
2018-06-25 14:54:25 +1000

    Added toggle all complete function

--- App.js
+++ App.js
@@ -15,11 +15,25 @@ export default class App extends Component<Props> {
   constructor(props){
     super(props);
     this.state = {
+      allComplete: false,
       value: "",
       items: [],
     };
     this.handleAddItem = this.handleAddItem.bind(this)
+    this.handleToggleAllComplete = this.handleToggleAllComplete.bind(this)
   }
+  handleToggleAllComplete() {
+    const complete = !this.state.allComplete;
+    const newItems = this.state.items.map((item) => ({
+      ...item,
+      complete
+    }));
+    this.setState({
+      items: newItems,
+      allComplete: complete
+    })
+  }
+
   handleAddItem() {
     if(!this.state.value) return;
     const newItems = [
@@ -43,6 +57,7 @@ export default class App extends Component<Props> {
         value={this.state.value}
         onAddItem={this.handleAddItem}
         onChange={(value) => this.setState({ value })}
+        onToggleAllComplete={this.handleToggleAllComplete}
         />
 
         <View style={styles.content}>
